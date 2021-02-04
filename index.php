8318c8996c7f9aa7f270a38b1117b3dc767bb5e8
progectja <78018684+progectja@users.noreply.github.com>
2021-02-04 12:25:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel="stylesheet" href="css/headerseg.css">
+	<link rel="stylesheet" href="css/common.css">
+	<link rel="stylesheet" href="https://progectja.github.io/css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+	
+
+
+
+  </head>
+  <body>
+   <?php include ".header.html" ?>
+   <?php include ".services.html" ?>
+
+</body>
+</html>
